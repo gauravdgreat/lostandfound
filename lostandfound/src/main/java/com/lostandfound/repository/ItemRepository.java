cabd2f40b9497ffdcf338d24ec37836b93e12f75
Gaurav Kumar Choudhary <97228474+gauravdgreat@users.noreply.github.com>
2024-06-25 16:59:22 +0530

    Update ItemRepository.java

--- lostandfound/src/main/java/com/lostandfound/repository/ItemRepository.java
+++ lostandfound/src/main/java/com/lostandfound/repository/ItemRepository.java
@@ -9,8 +9,5 @@ import java.util.List;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 	List<Item> findByNameContainingIgnoreCase(String name);
-	  List<Item> findAllByImageUrlNotNull();
-
-	    // Query method to find all found items with null imageUrl
-	    List<Item> findAllByImageUrlNull();
+	
 }
